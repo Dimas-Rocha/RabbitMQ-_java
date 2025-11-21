ea7e4358d1a4adc7ce6090c0c921c359b10bdfeb
Dimas Rocha <dimas.dasp@gmail.com>
2025-11-20 21:00:45 -0300

    Service

--- src/main/java/com/rmq/example/publisher/service/PublisherService.java
+++ src/main/java/com/rmq/example/publisher/service/PublisherService.java
@@ -1,4 +1,18 @@
 package com.rmq.example.publisher.service;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PublisherService {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    public void   publishText(String message, String queueName){
+        System.out.println(message);
+        rabbitTemplate.convertAndSend(queueName, message);
+
+    }
 }
